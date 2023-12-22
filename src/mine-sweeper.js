3dabb126962a6b67d07ae190742ed680aae9ccdd
anastshak <nastyashakura@gmail.com>
2023-12-22 22:01:52 +0300

    feat: add solutions

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    result.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      let cnt = 0;
+
+      if(i>0){
+        if(matrix[i-1][j]){
+          cnt += 1;
+        }
+        if(matrix[i-1][j+1]){
+          cnt += 1;
+        }
+        if(matrix[i-1][j-1]){
+          cnt += 1;
+        }
+      }
+
+      if(i < matrix.length - 1){
+        if(matrix[i+1][j]){
+          cnt +=1;
+        }
+        if(matrix[i+1][j+1]){
+          cnt += 1;
+        }
+        if(matrix[i+1][j-1]){
+          cnt +=1;
+        }
+      }
+
+      if(matrix[i][j+1]){
+        cnt +=1;
+      }
+      if(matrix[i][j-1]){
+        cnt +=1;
+      }
+      
+      result[i].push(cnt)
+    }    
+  }
+
+  return result;
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,7 +2,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement class DepthCalculator with method calculateDepth
- * that calculates deoth of nested array
+ * that calculates depth of nested array
  * 
  * @example
  * 
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let depth = 1;
+
+    for (let i = 0; i < arr.length; i++) {
+      if(Array.isArray(arr[i])){
+        let newDepth = this.calculateDepth(arr[i]) + 1;
+        depth = Math.max(depth, newDepth);
+      }      
+    }
+
+    return depth;
   }
 }
 
